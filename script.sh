2b92f04e21777bf7e5382d14ef43a9797bd3443a
Yúri Kerber <100870176+yurikerber@users.noreply.github.com>
2022-11-15 23:48:11 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
 sudo echo node_modules/ > .dockerignore
 
-
 echo ".dockerignore ~> Criado"
 echo 
 
-
 echo - Qual a porta desejada?
-read porta_imagem;
+read porta;
 echo 
 
 echo 'FROM node:16
@@ -14,13 +12,13 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN yarn install
 COPY . .
-EXPOSE $porta_imagem
+EXPOSE '$porta'
 CMD ["yarn", "serve"]' > Dockerfile
 
 echo - Qual o nome da imagem que deseja?
 read imagem_nome;
 
-docker build -t $imagem_nome .
+a=$(docker build -t $imagem_nome .)
 
 echo
 echo Imagem criada:
@@ -28,9 +26,20 @@ echo
 
 docker image ls $imagem_nome
 
-echo
-echo - Qual a porta do container?
-read porta_container;
-echo
+a=$(docker run -p 0.0.0.0:$porta:3000 -d --name $imagem_nome $imagem_nome)
 
-docker run -p 0.0.0.0:$porta_container:3000 -d --name $imagem_nome $imagem_nome
+echo
+echo Container criado:
+echo
+docker ps --filter name=$imagem_nome
+
+echo "
+_____________________________________
+|                  __               |
+|  /\ /\___ _ __  / /_   ___ _ __   |
+| / //_/ _ \ '_ \| '_ \ / _ \ '_ \  |
+|/ __ \  __/ |_) | (_) |  __/ |_) | |
+|\/  \/\___| .__/ \___/ \___| .__/  |
+|          |_|              |_|     |
+|___________________________________|"
+
